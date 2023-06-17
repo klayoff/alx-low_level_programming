e5bb75bb89af9d2ba3b985c7fa87bd34ab05deae
klayoff <anaskallouch@gmail.com>
2023-06-17 14:33:19 -0700

    Implement program to determine if a number is positive, negative, or zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,25 +5,27 @@
 /**
  * main - Entry point
  *
- * Description: Prints whether a randomly generated number is positive, negative, or zero
+ * Description: Prints whether a randomly generated number is
+ * positive, negative, or zero
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-    int n;
+	int  n;
 
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
-    printf("%d ", n);
+	printf("%d ", n);
 
-    if (n > 0)
-        printf("is positive\n");
-    else if (n == 0)
-        printf("is zero\n");
-    else
-        printf("is negative\n");
+	if (n > 0)
+		printf("is positive\n");
+	else if (n == 0)
+		printf("is zero\n");
+	else
+		printf("is negative\n");
 
-    return 0;
+	return (0);
 }
